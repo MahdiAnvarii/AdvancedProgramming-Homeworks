725d99ab64c2c9790708b27b82ecca97e1aa9288
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2025-04-19 00:28:01 +0330

    Implement test template creation system with validation
    - Added QuestionsTemplate struct to store question requirements
    - Created TestTemplate class with duplicate name validation
    - Enhanced splitTheOrder() to handle quoted template names
    - Implemented createTemplate() for processing template creation commands
    - Modified takeTheOrders() to support template creation workflow
    - Added proper error handling for duplicate template names
    - Maintained question processing while adding template functionality

--- a4/src/main.cpp
+++ a4/src/main.cpp
@@ -27,14 +27,14 @@ public:
     void setQuestion(string questionText_, string option1_, string option2_, string option3_, string option4_, int correctAnswer_, string difficulty_, string subject_){
         if (correctAnswer_ > 4 || correctAnswer_ < 1 || (find(VALID_DIFFICULTIES.begin(), VALID_DIFFICULTIES.end(), difficulty_) == VALID_DIFFICULTIES.end()))
             throw invalid_argument("Invalid question parameters");
-    questionText = questionText_;
-    option1 = option1_;
-    option2 = option2_;
-    option3 = option3_;
-    option4 = option4_;
-    correctAnswer = correctAnswer_;
-    difficulty = difficulty_;
-    subject = subject_;
+        questionText = questionText_;
+        option1 = option1_;
+        option2 = option2_;
+        option3 = option3_;
+        option4 = option4_;
+        correctAnswer = correctAnswer_;
+        difficulty = difficulty_;
+        subject = subject_;
     }
 private:
     string questionText;
@@ -47,12 +47,50 @@ private:
     string subject;
 };
 
+struct QuestionsTemplate{
+    string subject;
+    string difficulty;
+    int count;
+};
+
+class TestTemplate{
+public:
+    TestTemplate(string testTemplateName_, const vector<QuestionsTemplate>& potentialQuestions_, const vector<TestTemplate*>& testTemplates){
+        setTestTemplate(testTemplateName_, potentialQuestions_, testTemplates);
+    }
+    void setTestTemplate(string testTemplateName_, const vector<QuestionsTemplate>& potentialQuestions_, const vector<TestTemplate*>& testTemplates){
+        for (const auto t : testTemplates) {
+            if (t->testTemplateName == testTemplateName_)
+                throw invalid_argument("Duplicate name: \'" + testTemplateName_ + "\'");
+        }
+        testTemplateName = testTemplateName_;
+        potentialQuestions = potentialQuestions_;
+        cout << "Template \'" << testTemplateName_ << "\' was created successfully." << endl;
+    }
+private:
+    string testTemplateName;
+    vector<QuestionsTemplate> potentialQuestions;
+};
+
 vector<string> splitTheOrder(string order){
     istringstream iss(order);
     vector<string> orderToVector;
+    bool insideQuote = false;
     string word;
+    string temp;
     while (iss >> word) {
-        orderToVector.push_back(word);
+        if (word.front() == '\''){
+            temp = word.substr(1);
+            insideQuote = true;
+        } else if (insideQuote && word.back() == '\''){
+            temp += " " + word.substr(0, word.length() - 1);
+            orderToVector.push_back(temp);
+            insideQuote = false;
+        } else if (insideQuote) {
+            temp += " " + word;
+        } else {
+            orderToVector.push_back(word);
+        }
     }
     return orderToVector;
 }
@@ -63,7 +101,6 @@ vector<Question*> processQuestionsFile(const string questionsFileAddress){
     vector<Question*> questions;
     getline(questionsFile, questionsFileLine);
     while(getline(questionsFile, questionsFileLine)){
-        cout << questionsFileLine << endl;
         istringstream iss(questionsFileLine);
         string questionText, option1, option2, option3, option4, correctAnswer, difficulty, subject;
         getline(iss, questionText, ',');
@@ -81,16 +118,35 @@ vector<Question*> processQuestionsFile(const string questionsFileAddress){
     return questions;
 }
 
-void takeTheOrders(){
+void createTemplate(const vector<string>& orderToVector, vector<TestTemplate*>& testTemplates){
+    string testTemplateName = orderToVector[1].substr(2, orderToVector[1].size()-2);
+    vector<QuestionsTemplate> potentialQuestions;
+    for (int i=2; i<orderToVector.size(); i++){
+        istringstream iss(orderToVector[i]);
+        string subject, difficulty, count;
+        getline(iss, subject, ':');
+        getline(iss, difficulty, ':');
+        getline(iss, count, ':');
+        potentialQuestions.push_back({subject, difficulty, stoi(count)});
+    }
+
+    TestTemplate* testTemplate = new TestTemplate(testTemplateName, potentialQuestions, testTemplates);
+    testTemplates.push_back(testTemplate);
+}
+
+void takeTheOrders(const vector<Question*>& questions){
     string theOrder;
+    vector<TestTemplate*> testTemplates;
     while (getline(cin,theOrder)){
         vector<string> orderToVector = splitTheOrder(theOrder);
-
+        if (orderToVector[0] == CREATE_TEMPLATE){
+            createTemplate(orderToVector, testTemplates);
+        }
     }
 }
 
 int main(int argc, char* argv[]){
     const string questionFileAddress = argv[1];
     vector<Question*> questions = processQuestionsFile(questionFileAddress);
-    takeTheOrders();
+    takeTheOrders(questions);
 }
