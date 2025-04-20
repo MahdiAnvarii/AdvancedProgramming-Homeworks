9d1d7b33dcbe651dbc6c325968344409095ec9f8
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2025-04-20 17:05:19 +0330

    Implement test attendance system with question navigation
    - Added Question::printQuestion() for interactive question display
    - Implemented Test::selectQuestionsForTest() and Test::sortTestQuestions()
    - Created attendToTest() command handler with navigation support
    - Added PREVIOUS and VALID_ANSWERS constants for answer validation
    - Enhanced splitTheOrder() to handle quoted strings better
    - Added answer tracking in Question class
    - Implemented previous question navigation functionality
    - Improved empty input handling in takeTheOrders()
    - Added test name validation for attendance

--- a4/src/main.cpp
+++ a4/src/main.cpp
@@ -17,6 +17,14 @@ const string TEST = "test";
 const string TESTS = "tests";
 const string SUBJECT = "subject";
 const vector<string> VALID_DIFFICULTIES = {"easy", "medium", "hard"};
+const string PREVIOUS = "previous";
+const vector<string> VALID_ANSWERS = {"1", "2", "3", "4", "", "previous"};
+
+struct QuestionsTemplateStruct{
+    string subject;
+    string difficulty;
+    int count;
+};
 
 class Question{
 public:
@@ -28,10 +36,12 @@ public:
         if (correctAnswer_ > 4 || correctAnswer_ < 1 || (find(VALID_DIFFICULTIES.begin(), VALID_DIFFICULTIES.end(), difficulty_) == VALID_DIFFICULTIES.end()))
             throw invalid_argument("Invalid question parameters");
         questionText = questionText_;
-        option1 = option1_;
-        option2 = option2_;
-        option3 = option3_;
-        option4 = option4_;
+        vector<string> options_;
+        options_.push_back(option1_);
+        options_.push_back(option2_);
+        options_.push_back(option3_);
+        options_.push_back(option4_);
+        options = options_;
         correctAnswer = correctAnswer_;
         difficulty = difficulty_;
         subject = subject_;
@@ -46,16 +56,41 @@ public:
         numOfCorrect = numOfCorrect_;
         priority = 3*numOfIncorrect_ + 1*numOfBlank_ - 2*numOfCorrect_;
     }
+    string printQuestion(int questionCounter, bool isTheAnswerPrevious){
+        cout << questionCounter << ") " << questionText << endl;
+        int answerIndex = -1;
+        if (isTheAnswerPrevious){
+            if (theAnswer == "1") answerIndex = 0;
+            else if (theAnswer == "2") answerIndex = 1;
+            else if (theAnswer == "3") answerIndex = 2;
+            else if (theAnswer == "4") answerIndex = 3;
+        } 
+        for (int i=0; i<4; ++i) {
+            cout << "    " << (i+1) << ". " << options[i];
+            if (i == answerIndex) {
+                cout << " <-";
+            }
+            cout << endl;
+        }
+        cout << "Your answer: ";
+        string theAnswer_;
+        getline(cin, theAnswer_);
+        while ((find(VALID_ANSWERS.begin(), VALID_ANSWERS.end(), theAnswer_) == VALID_ANSWERS.end()) || (theAnswer_ == PREVIOUS && questionCounter==1)){
+            cout << "Invalid answer, please try again." << endl;
+            cout << "Your answer: ";
+            getline(cin, theAnswer_);
+        }
+        theAnswer = theAnswer_;
+        cout << endl;
+        return theAnswer_;
+    }
     string getQuestionText() const { return questionText; }
     string getSubject() const { return subject; }
     string getDifficulty() const { return difficulty; }
     int getPriority() const { return priority; }
 private:
     string questionText;
-    string option1;
-    string option2;
-    string option3;
-    string option4;
+    vector<string> options;
     int correctAnswer;
     string difficulty;
     string subject;
@@ -63,12 +98,7 @@ private:
     int numOfBlank;
     int numOfCorrect;
     int priority;
-};
-
-struct QuestionsTemplateStruct{
-    string subject;
-    string difficulty;
-    int count;
+    string theAnswer;
 };
 
 class TestTemplate{
@@ -104,25 +134,6 @@ void sortQuestions(vector<Question*>& questions, bool needPriority){
     });
 }
 
-vector<Question*> selectQuestionsForTest(const TestTemplate& testTemplate_, const vector<Question*> questions){
-    vector<Question*> testQuestions;
-    vector<QuestionsTemplateStruct> potentialQuestions = testTemplate_.getPotentialQuestions();
-    for (const auto& potentialQuestion : potentialQuestions){
-        int theCount = potentialQuestion.count;
-        int theCounter = 0;
-        string theSubject = potentialQuestion.subject;
-        string theDifficulty = potentialQuestion.difficulty;
-        for (const auto& question : questions){
-            if (theCounter >= theCount) break;
-            else if (theSubject == question->getSubject() && theDifficulty == question->getDifficulty()){
-                testQuestions.push_back(question);
-                theCounter+=1;
-            }
-        }
-    }
-    return testQuestions;
-}
-
 class Test{
 public:
     Test(string testName_, const TestTemplate& testTemplate_, const vector<Question*>& questions)
@@ -135,6 +146,29 @@ public:
         testTemplate = testTemplate_;
         testQuestions = testQuestions_;
         cout << "Test \'" << testName_ << "\' was generated successfully." << endl;
+    }
+    vector<Question*> selectQuestionsForTest(const TestTemplate& testTemplate_, const vector<Question*> questions){
+        vector<Question*> testQuestions;
+        vector<QuestionsTemplateStruct> potentialQuestions = testTemplate_.getPotentialQuestions();
+        for (const auto& potentialQuestion : potentialQuestions){
+            int theCount = potentialQuestion.count;
+            int theCounter = 0;
+            string theSubject = potentialQuestion.subject;
+            string theDifficulty = potentialQuestion.difficulty;
+            for (const auto& question : questions){
+                if (theCounter >= theCount) break;
+                else if (theSubject == question->getSubject() && theDifficulty == question->getDifficulty()){
+                    testQuestions.push_back(question);
+                    theCounter+=1;
+                }
+            }
+        }
+        return testQuestions;
+    }
+    string getTestName() const { return testName; }
+    vector<Question*> getTestQuestions() const { return testQuestions; }
+    void sortTestQuestions(){
+        sortQuestions(testQuestions, false);
     }
 private:
     string testName;
@@ -149,7 +183,10 @@ vector<string> splitTheOrder(string order){
     string word;
     string temp;
     while (iss >> word) {
-        if (word.front() == '\''){
+        if (word.front() == '\'' && word.back() == '\''){
+            orderToVector.push_back(word.substr(1, word.length() - 2));
+        }
+        else if (word.front() == '\''){
             temp = word.substr(1);
             insideQuote = true;
         } else if (insideQuote && word.back() == '\''){
@@ -206,7 +243,7 @@ void createTemplate(const vector<string>& orderToVector, vector<TestTemplate*>& 
     testTemplates.push_back(testTemplate);
 }
 
-void generateTest(const vector<string>& orderToVector, vector<Question*>& questions, const vector<TestTemplate*>& testTemplates, vector<Test*> tests){
+void generateTest(const vector<string>& orderToVector, vector<Question*>& questions, const vector<TestTemplate*>& testTemplates, vector<Test*>& tests){
     string testName = orderToVector[1];
     string testTemplateName = orderToVector[2];
     sortQuestions(questions, true);
@@ -223,7 +260,39 @@ void generateTest(const vector<string>& orderToVector, vector<Question*>& questi
     }
 
     Test* test = new Test(testName, *testTemplate, questions);
+    test->sortTestQuestions();
     tests.push_back(test);
+}
+
+void attendToTest(const vector<string>& orderToVector, vector<Question*>& questions, vector<Test*>& tests){
+    string testName = orderToVector[1];
+    bool isTestNameValid = false;
+    Test* test = nullptr;
+    for (const auto& t : tests) {
+        if (t->getTestName() == testName){
+            isTestNameValid = true;
+            test = t;
+        }
+    }
+    if (!isTestNameValid){
+        throw invalid_argument("Could not find test: \'" + testName + "\'" + "\n");
+    }
+    cout << test->getTestName() << ":" << endl << endl;
+    int questionCounter = 0;
+    vector<Question*> testQuestions = test->getTestQuestions();
+    string theAnswer;
+    bool isTheAnswerPrevious = false;
+    while (questionCounter < testQuestions.size()){
+        theAnswer = testQuestions[questionCounter]->printQuestion(questionCounter+1, isTheAnswerPrevious);
+        if (theAnswer == PREVIOUS){
+            questionCounter-=1;
+            isTheAnswerPrevious = true;
+        } else {
+            questionCounter+=1;
+            isTheAnswerPrevious = false;
+        }
+    }
+    cout << "Finished " << test->getTestName() << " ." << endl;
 }
 
 void takeTheOrders(vector<Question*>& questions){
@@ -231,11 +300,15 @@ void takeTheOrders(vector<Question*>& questions){
     vector<TestTemplate*> testTemplates;
     vector<Test*> tests;
     while (getline(cin,theOrder)){
+        if (theOrder.empty()) continue;
         vector<string> orderToVector = splitTheOrder(theOrder);
+        if (orderToVector.empty()) continue;
         if (orderToVector[0] == CREATE_TEMPLATE){
             createTemplate(orderToVector, testTemplates);
         } else if (orderToVector[0] == GENERATE_TEST){
             generateTest(orderToVector, questions, testTemplates, tests);
+        } else if (orderToVector[0] == ATTEND){
+            attendToTest(orderToVector, questions, tests);            
         }
     }
 }
